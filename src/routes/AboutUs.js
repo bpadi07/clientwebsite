fbb5328fbac6002531273c668a825aa83da885c0
minal <minalsutar2@gmail.com>
2023-02-23 18:42:52 +0530

    aboutus changes

--- src/routes/AboutUs.js
+++ src/routes/AboutUs.js
@@ -46,13 +46,15 @@ const AboutUs = () => {
                 </li>
              </ul>
              </div>
+
+             <div className="aboutus-oulist">
+              <h4>Unordered & Ordered Lists</h4><br></br>
+              <pre className="fa fa-thumbs-up" style-prop-object="font-size:50px" color="yellow">           Online Courses with full discount systems.</pre><br></br><br></br>
+              <pre className="fa fa-thumbs-up" style-prop-object="font-size:50px" color="yellow">           Online Certificates which can be used worldwide</pre>
+              </div>
             <div className="col1-img">
               <img height="300" width="400" src="https://codenovas.com/wp-content/uploads/2018/05/course_image-2-1024x682.jpg" alt="computer lmg"/>
             </div>
-             
-               
-
-
     </div>
     <div  className="col2">
        <img  width="500" height="500"src="https://codenovas.com/wp-content/uploads/2018/07/cat_2-1-1024x684.jpg" alt="painting"/>
@@ -81,8 +83,95 @@ const AboutUs = () => {
       </div>
   </div>   
   </div>
-  
+  <br></br>
+  <br></br>
+  <hr></hr>
+
+    <div  className="aboutus-certification">
+    <br></br>
+    <h3>Certifications</h3>
+    </div>
+<div className="main-div2">
+        <div className="col3">
+          <img  height="300" width="400" src="https://codenovas.com/wp-content/uploads/2018/07/certificate-2-480x380.png" alt="g-certificate"/><br></br>
+           
+          <h4>Google Certified</h4>       
+        </div>
+        <div className="col4">
+          <img height="300" width="400" src="https://codenovas.com/wp-content/uploads/2018/07/certificate-2-480x380.png" alt="m-certificate"/><br></br>
+          
+          <h4>Microsoft Certified</h4>
+        </div>    
+        <div className="col5">
+          <img height="300" width="400" src="https://codenovas.com/wp-content/uploads/2018/07/certificate-2-480x380.png" alt="a-certificate"/><br></br>
+          <h4>Apple Certified</h4>
+          </div>
+</div>
+<div className="main-div3">
+        <div className="col6">
+          <a href="https://facebook.com" target="_main" title="share on facebook">
+           <button  type="submit"  className="fsubmit">
+         <div className="fbutton">
+           <img className="fimage" style-prop-object="vertical-align:middle" height="15px" width="15px" src="https://static.xx.fbcdn.net/rsrc.php/v3/yn/r/GzgedhmzSQa.png" alt="fimage"/>
+           <span className="text">Share</span>
+           <span className="zero">0</span>
+        </div>
+          </button>
+          </a> 
+        </div>
+        <div className="col7"> 
+          <a href="https://twitter.com" title="share on twitter" target="_main">
+          <button type="submit" className="tsubmit">
+            <div className="tbutton">
+              <i  className="fa fa-twitter"></i> 
+              <span className="ttext">   Tweet</span>
+            </div>
+            </button>
+          </a>
+        </div>
+</div> 
+
+<div className="main-div4">
+    <div className="col8">
+       <div>
+        <h4>About</h4><br></br>
+        </div>
+        <div>
+        <p>Code nova is an educational and sport funs website for Guest, Subscriber, Collaborator and Student either to learn or teach the whole course or use specific section of the course as a reference via video or text or sport fans to watch live or highlight NFL and Premier League games.</p>
+        <br></br></div>
+        <div>
+        <a href="https://facebook.com" className="fa fa-facebook"></a> 
+        <a href="https://twitter.com"  className="fa fa-twitter"></a>
+        <a href="https://instaram.com" className="fa fa-instagram"></a>
+        </div>  
+    </div>
+    <div className="col9">
+        <div>
+        <h4>Contact</h4><br></br>
+        </div>
+        <div>
+        <p>PO Box 1903, Snellville, GA 30078 <br></br>Tel.: +1 425 435 7970<br></br>Fax: +1 781 2283160<br></br>support@codenovas.com</p>
+        </div>
+    </div>
+    <div className="col10">
+        <div>
+        <h4>Pages</h4>
+        </div>
+        <div>
+        <ul>
+          <li>Home</li>
+          <li>Services</li>
+          <li>Courses</li>
+          <li>AboutUs</li>
+          <li>Login</li>
+        </ul>
+        </div>
+    </div>     
+</div>
+
+    
 </>
+
   );
   
   
